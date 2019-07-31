8c424f393a6b467b8a7f87d610a44ea7a35896dc
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-07-31 23:11:07 +0300

    update Header

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar toggleable="lg" type="dark" variant="dark">
             <b-container>
                 <b-navbar-brand href="#">IT Serve</b-navbar-brand>
 
