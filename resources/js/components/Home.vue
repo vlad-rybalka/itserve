2eeffcc9e6d539121913cc5084d01cbae1a40631
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-07-31 23:11:19 +0300

    update Home Page

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,3 +1,43 @@
 <template>
-    <div>Home</div>
+    <div class="mt-3">
+        <b-row>
+            <b-col>
+                <h3 class="float-left pt-2">Posts</h3>
+                <b-button variant="success" class="float-right">+ Add post</b-button>
+            </b-col>
+        </b-row>
+        <b-row class="mt-2">
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+            
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+
+            <PostItem/>
+            <PostItem/>
+            <PostItem/>
+        </b-row>
+        <div class="overflow-auto">
+            <b-pagination-nav :link-gen="linkGen" :number-of-pages="10" use-router align="center"></b-pagination-nav>
+        </div>
+    </div>
 </template>
+
+<script>
+import PostItem from './PostItem'
+export default {
+    components: {
+          PostItem
+        },
+}
+</script>
