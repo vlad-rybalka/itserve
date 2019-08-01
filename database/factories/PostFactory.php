83450468b13e8d0c7e695c60851d9362233d4ffc
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-02 02:41:41 +0300

    add Post seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Post;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        'title' => $faker->name,
+        'description' => $faker->text,
+        'img' => 'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    ];
+});
