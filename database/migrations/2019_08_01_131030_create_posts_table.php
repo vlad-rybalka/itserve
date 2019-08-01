cb6dda70c448189f6be97e8214b58938eb1670e0
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-01 22:17:04 +0300

    update posts-table migration

--- database/migrations/2019_08_01_131030_create_posts_table.php
+++ database/migrations/2019_08_01_131030_create_posts_table.php
@@ -17,7 +17,6 @@ class CreatePostsTable extends Migration
             $table->bigIncrements('id');
             $table->string('title');
             $table->text('description');
-            $table->dateTime('date');
             $table->string('img');
             $table->timestamps();
         });
