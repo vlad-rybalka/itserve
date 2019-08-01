7ff8c6531e0b9dbc450a19275b843c82b9707933
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-01 22:43:36 +0300

    add props on PostItem

--- resources/js/components/PostItem.vue
+++ resources/js/components/PostItem.vue
@@ -7,14 +7,13 @@
             <b-row no-gutters>
                 <b-col md="12">
                     <div class="img_wrap">
-                        <b-card-img src="https://avatars.mds.yandex.net/get-pdb/788379/1bfafa86-d6e6-4570-bfdf-1e43066fb20d/s375" class="rounded-0"></b-card-img>
+                        <b-card-img :src="img" class="rounded-0"></b-card-img>
                     </div>
                 </b-col>
                 <b-col md="12">
-                    <b-card-body title="Horizontal Card" class="p-0 mt-3">
+                    <b-card-body :title="title" class="p-0 mt-3">
                         <b-card-text>
-                            This is a wider card with supporting text as a natural lead-in to additional content.
-                            This content is a little bit longer.
+                            {{description}}
                         </b-card-text>
                     </b-card-body>
                 </b-col>
@@ -33,6 +32,7 @@ export default {
     components: {
           Header
         },
+    props: ['title', 'description', 'img'],
     methods:{
         OnClick: function(){
             console.log(123);
