29d961ce649c60fa1b28543d231ed7b8def3c1be
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-04 02:35:25 +0300

    update AddPostPage

--- resources/js/views/AddPostPage.vue
+++ resources/js/views/AddPostPage.vue
@@ -4,25 +4,33 @@
         <b-card bg-variant="light" class="mt-3">
             <b-row>
                 <b-col cols="8">
-                    <b-form-group label-cols="4" label-cols-lg="2" label="Headline" label-for="input-headline">
-                        <b-form-input id="input-headline"></b-form-input>
+                    <b-form-group label-cols="4" label-cols-lg="2" label="Title" label-for="input-title" >
+                        <b-form-input id="input-title" placeholder="Title" aria-describedby="input-title-feedback" v-model="formData.title" :state="displayErrors.title"></b-form-input>
+                        <small class="text-danger">{{getError('title')}}</small>
                     </b-form-group>
-                    <b-form-group label-cols="4" label-cols-lg="2" label="Text" label-for="input-text">
-                        <b-form-textarea id="input-text"></b-form-textarea>
+                    <b-form-group label-cols="4" label-cols-lg="2" label="Description" label-for="input-description" >
+                        <b-form-textarea id="input-description" placeholder="Description" aria-describedby="input-description-feedback"  v-model="formData.description" :state="displayErrors.description"></b-form-textarea>
+                        <small class="text-danger">{{getError('description')}}</small>
                     </b-form-group>
-                    <b-form-group label-cols="4" label-cols-lg="2" label="Image" label-for="input-image">
+                    <b-form-group label-cols="4" label-cols-lg="2" label="Image" label-for="input-file">
                         <b-form-file
-                            :state="Boolean(file)"
+                            id="input-file"
+                            :state="displayErrors.file"
                             placeholder="Choose a file..."
                             drop-placeholder="Drop file here..."
+                            aria-describedby="input-file-feedback"
+                            accept="image/*"
+                            @change="onFileChange"
+                            v-model="formData.file"
                         >
                         </b-form-file>
+                        <small class="text-danger">{{getError('file')}}</small>
                     </b-form-group>
-                     <b-button variant="primary" class="px-5 float-right">Add post</b-button>
+                     <b-button variant="primary" class="px-5 float-right" @click="onSubmit">Add post</b-button>
                 </b-col>
                 <b-col cols="4">
                     <div class="img_wrap">
-                        <img src="/img/no-image.jpg" alt="" width="100%">
+                        <img id="imageid" src="/img/no-image.jpg" alt="" width="100%">
                     </div>
                 </b-col>
             </b-row>
@@ -32,19 +40,64 @@
 
 <script>
     import PostsApi from '../services/api/posts'
+    import {mapGetters, mapActions} from 'vuex'
+
     export default {
         data() {
-        return {
-            form: {
-                headline: '',
-                text: '',
-                file: null,
+            return {
+                formData: {
+                    title: '',
+                    description: '',
+                    file: null
+                },
+                fileUrl: "",
+                displayErrors: {
+                    title: null,
+                    description: null,
+                    file: null
+                }
             }
-        }
+        },
+        computed:{
+             ...mapGetters(['errors']),
         },
         methods: {
+            ...mapActions(['createPost']),
+            onFileChange(event){
+                var input = event.target;
+
+                if (input.files && input.files[0]) {
+                    var reader = new FileReader();
+                    reader.onload = function(e) {
+                        this.fileUrl = e.target.result;
+                        document.getElementById("imageid").src=this.fileUrl;
+                    }
+                    reader.readAsDataURL(input.files[0]);
+                }
+            },
+            getError(name){
+                if(this.errors[name]) return this.errors[name][0];
+                    return ""
+            },
+            showErrors(){
+                for (var key in this.displayErrors) {
+                    this.displayErrors[key]  = !this.errors.hasOwnProperty(key);
+                }
+            },
+            async onSubmit(){
+                this.createPost(this.formData).then(() => {
+                    alert("Post created!")
+                    this.$router.push({ name: "posts"})
+
+                }).catch(error => {
+                    this.showErrors()
+                })
+                
+            }
         
-        
+        },
+        mounted(){
+            this.$store.commit('updateErrors', {})
         }
     }
 </script>
@@ -55,13 +108,11 @@
     }
     .img_wrap {
         width: 336px;
-        height: 260px;
         overflow: hidden;
     }
 
     .img_wrap img {
         min-width: 336px;
-        min-height: 260px;
         vertical-align: middle;
     }
 </style>
