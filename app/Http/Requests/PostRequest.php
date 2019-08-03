95a51b10980c09056c95a28e607f1be6d8a5eb9b
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-04 02:34:03 +0300

    update PostRequest add Validation

--- app/Http/Requests/PostRequest.php
+++ app/Http/Requests/PostRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\JsonResponse;
 
 class PostRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class PostRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,10 @@ class PostRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|string|min:10|max:200',
+            'description' => 'required|min:250|max:2000',
+            'file' => 'required|image'
         ];
     }
+
 }
