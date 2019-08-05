667f0682e4e38f0f9b3b819d30e8e6bce31ec626
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-05 06:45:28 +0300

    update App add showMessage

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
         <Header></Header>
+        
         <b-container>
-            <router-view></router-view>
+            <router-view @message="data => showMessage(data)"></router-view>
         </b-container>
+        
     </div>
 </template>
 
@@ -11,7 +13,31 @@
 import Header from './Header'
 export default {
     components: {
-          Header
-        },
+        Header
+    },
+    methods:{
+        showMessage(data) {
+            this.$bvToast.toast(data.body, {
+                title: data.title,
+                appendToast: false,
+                variant: 'success',
+            })
+        }
+    }
+
 }
 </script>
+
+<style>
+.b-toaster-top-right{
+    position: fixed !important;
+    top: 20px  !important;
+    right: 20px  !important;
+    width: 300px;
+}
+.toast{
+    opacity: 1 !important;
+    margin-bottom: 
+}
+</style>
+
