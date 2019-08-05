84f16908b98b81290263c4169b1ef1a000a8e28c
vlad-rybalka <42077075+vlad-rybalka@users.noreply.github.com>
2019-08-05 06:44:41 +0300

    update Header add links to login and register

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -10,14 +10,22 @@
 
                 <b-collapse id="nav-collapse" is-nav>
                 <b-navbar-nav>
-                    <b-nav-item href="#">Link</b-nav-item>
+                    <router-link :to="{name: 'posts'}">
+                        <p class="nav-link mb-0"> Posts</p>
+                    </router-link>
                 </b-navbar-nav>
-
-                <b-navbar-nav class="ml-auto">
+                <b-navbar-nav class="ml-auto"  v-if="!isAuth">
+                    <router-link :to="{name: 'login'}">
+                        <p class="nav-link mb-0"> Sign in</p>
+                    </router-link>
+                    <router-link :to="{name: 'register'}">
+                        <p class="nav-link mb-0"> Sign up</p>
+                    </router-link>  
+                </b-navbar-nav>
+                <b-navbar-nav class="ml-auto"  v-if="isAuth">
                     <b-nav-item-dropdown right>
-                    <template slot="button-content"><em>User</em></template>
-                    <b-dropdown-item href="#">Profile</b-dropdown-item>
-                    <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+                        <template slot="button-content"><em>{{user.nickname}}</em></template>
+                        <b-dropdown-item @click="onSignOutClick">Sign Out</b-dropdown-item>
                     </b-nav-item-dropdown>
                 </b-navbar-nav>
                 </b-collapse>
@@ -25,3 +33,26 @@
         </b-navbar>
     </div>
 </template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+export default {
+    computed:{
+        ...mapGetters('auth',['user', 'isAuth'])
+    },
+    methods:{
+        ...mapActions('auth',['logout']),
+        async onSignOutClick(){
+            await this.logout();
+            this.$router.push({ name: "home"})
+        }
+    }
+}
+</script>
+
+<style>
+a:hover{
+    text-decoration: none;
+}
+</style>
+
